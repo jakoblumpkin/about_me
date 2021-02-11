31830faf73eb2edd696cf85af8671054f8a0c78a
jakoblumpkin <lumpkinjakobr@gmail.com>
2021-02-11 11:26:36 -0600

    added comments

--- java.js
+++ java.js
@@ -6,6 +6,7 @@ var correctA=0;
 let name=prompt("What's your name? : ")
 alert("Hey "+name+" Welcome to my biography page.");
 
+//1st Question
 let firstQ=prompt(name+" Can I ask you a few questions? yes or no: ");
 if (firstQ.toLowerCase()==="yes"){
     var correctA=correctA+1;
@@ -14,6 +15,7 @@ if (firstQ.toLowerCase()==="yes"){
     alert("I suggest you leave the page!");
 }
 
+//2nd Question
 let secondQ=prompt("Is 'jacob' how I spell my first name: yes or no: ");
 if (secondQ.toLowerCase()==="yes"){
     alert("No it's 'Jakob' ");
@@ -22,6 +24,8 @@ if (secondQ.toLowerCase()==="yes"){
     alert("Correct it's Jakob");
 }
 
+
+//3rd Question
 let thirdQ=prompt("Did I go to College?: yes or no: ");
 if (thirdQ.toLowerCase()==="yes"){
     alert("Nope I didn't. Nice try " +name);
@@ -30,6 +34,8 @@ if (thirdQ.toLowerCase()==="yes"){
     alert("Correct");
 }
 
+
+//4th Question
 let fourthQ=prompt("Am I younger than 25 years old?: yes or no: ");
 if (fourthQ.toLowerCase()==="yes"){
     var correctA=correctA+1;
@@ -38,6 +44,8 @@ if (fourthQ.toLowerCase()==="yes"){
     alert("Nice try but I'm 24 years old.");
 }
 
+
+//5th Question
 let fifthQ=prompt("Just to check if your paying attention. Is 'jacob' how I spell my first name?: yes or no: ");
 if (fifthQ.toLowerCase()==="yes"){
     alert("No it's 'Jakob' ");
@@ -49,7 +57,7 @@ if (fifthQ.toLowerCase()==="yes"){
 alert("Thanks for your time ",name);
 
 
-
+//6th Question
 for(let i=0; i<6; i++){
     let answer=prompt("How old do you think I am?: ");
     if (answer==24){
@@ -67,7 +75,7 @@ for(let i=0; i<6; i++){
     }
 }
 
-
+//7th Question
 for(let i=0; i<6; i++){
     let answer=prompt("What is one of my favorite call of duties gmaes?");
     if(answer.toLowerCase()=="black ops" || answer.toLowerCase()=="modern warfare 2" || answer.toLowerCase()=="modern warfare"){
